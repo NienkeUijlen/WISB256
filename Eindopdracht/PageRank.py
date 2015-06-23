61042b93524df67c03cf20f56bc2d53a1c920a1b
nienkeuijlen <n.m.uijlen@students.uu.nl>
2015-06-23 10:32:27 +0000

    functie gedefinieerd

--- Eindopdracht/PageRank.py
+++ Eindopdracht/PageRank.py
@@ -3,22 +3,34 @@ import urllib.request
 import re
 from re import findall
 
-domain = "uu.nl"
-myurl = "http://" + domain
-#input("@> ")
-x = urllib.request.urlopen(myurl)
-for i in re.findall('''href=["'](.[^"']+)["']''', str(x.read()), re.I):
-#    print("hoofdpagina")
-    if re.search("css$", i) == None and re.search("png$", i) == None and re.search("ico$", i) == None and re.search("/rss/", i) == None:
-        if re.match("/", i):
-            i = myurl + i
-            print(i)
-        
-        elif re.search(domain, i):
-            print(i)
-        else:
-            pass
+Lijst=[]
+
+def JoriEnNienkesSuperfunctieEnTinkaIsErNiet(domein):
+    myurl = "http://www." + domein
+    x = urllib.request.urlopen(myurl)
+    for i in re.findall('''href=["'](.[^"']+)["']''', str(x.read()), re.I):
+        if re.search("css$", i) == None and re.search("png$", i) == None and re.search("ico$", i) == None and re.search("/rss/", i) == None:
+            if re.search(domein, i) == None:
+                if re.match("/", i) == None:
+                    i = "/" + i
+                i = myurl + i
+                if (i in Lijst) == False:
+                    Lijst.append(i)
+                
+            elif re.search(domein, i):
+                if (i in Lijst) == False:
+                    Lijst.append(i)
+            else:
+                pass
+            
+JoriEnNienkesSuperfunctieEnTinkaIsErNiet("boswell-beta.nl")
+
+print(Lijst)
+
+for i in Lijst:
+    JoriEnNienkesSuperfunctieEnTinkaIsErNiet(i)
   
+print(Lijst)
     
     # try:
     #     for j in re.findall('''href=["'](.[^"']+)["']''', str(urllib.request.urlopen(str(i)).read()), re.I):
