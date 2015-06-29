5f9bbb1b283d6d06576cd36acbbe8dd70beaf4ba
nienkeuijlen <n.m.uijlen@students.uu.nl>
2015-06-29 10:56:09 +0000

    Matrix gemaakt

--- Eindopdracht/PageRank2.py
+++ Eindopdracht/PageRank2.py
@@ -2,16 +2,20 @@ import urllib
 import urllib.request
 import re
 from re import findall
+from scipy.sparse import coo_matrix
+import numpy as np
 
 
 X=[]
-Lijst=['http://www.verseoogst.nl/']
-domein = 'verseoogst.nl'
+row=[]
+col=[]
+Lijst=['http://www.madurodam.nl/']
+domein = 'madurodam.nl'
 def JoriEnNienkesSuperfunctieEnTinkaIsErNiet(myurl):
     try:
         x = urllib.request.urlopen(myurl)
         for i in re.findall('''href=["'](.[^"']+)["']''', str(x.read()), re.I):
-            if re.search("css$", i) == None and re.search("png$", i) == None and re.search("ico$", i) == None and re.search("/rss/", i) == None and re.search(".pdf$", i) == None:
+            if re.search("css", i) == None and re.search("png$", i) == None and re.search("ico$", i) == None and re.search("/rss/", i) == None and re.search(".pdf$", i) == None and re.search("facebook", i)==None and re.search("twitter", i)==None and re.search("mailto", i)==None and re.search("google", i)==None and re.search(" ", i)==None:
                 if (re.search(domein, i) == None) and (re.match('www', i)==None) and (re.match('http', i) == None):
                     if re.match("/", i) == None:
                         i = "/" + i
@@ -21,7 +25,8 @@ def JoriEnNienkesSuperfunctieEnTinkaIsErNiet(myurl):
                     a = (Lijst.index(myurl),Lijst.index(i))
                     if (a in X) == False:
                         X.append(a)
-                        
+                        row.append(Lijst.index(myurl))
+                        col.append(Lijst.index(i))
                         
                 elif re.search(domein, i):
                     
@@ -30,12 +35,13 @@ def JoriEnNienkesSuperfunctieEnTinkaIsErNiet(myurl):
                     a = (Lijst.index(myurl),Lijst.index(i))
                     if (a in X) == False:
                         X.append(a)
-                        
+                        row.append(Lijst.index(myurl))
+                        col.append(Lijst.index(i))
                         
     except:
         pass
     
-JoriEnNienkesSuperfunctieEnTinkaIsErNiet('http://www.verseoogst.nl/')
+JoriEnNienkesSuperfunctieEnTinkaIsErNiet('http://www.madurodam.nl/')
 
 print(Lijst)
 print(len(Lijst))
@@ -43,9 +49,21 @@ print(len(Lijst))
 for i in Lijst:
     print(i)
     JoriEnNienkesSuperfunctieEnTinkaIsErNiet(i)
-  
 for j in Lijst:
     print(str(Lijst.index(j))+ " "+ str(j))
 print(len(Lijst))
-print(X)
+print(X[-1])
 print(len(X))
+
+data = len(row)*[1]
+v = len(Lijst)*[1/len(Lijst)]
+
+matrix = coo_matrix((data, (row, col)), shape=(len(Lijst), len(Lijst))).toarray()
+
+print(matrix)
+
+for i in range(10):
+    v = matrix.dot(v)
+    som = sum(v)
+    v = (1/som)*v
+    print(i,v)
